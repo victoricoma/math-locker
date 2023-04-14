e7e852b43c4438b23b5a8387b5731e4c6573ea1e
victoricoma <victorsena3010@gmail.com>
2023-04-13 22:43:24 -0300

    LCD e organização para Mathlooker

--- MathLocker/MathLocker.ino
+++ MathLocker/MathLocker.ino
@@ -1,94 +1,93 @@
-#include <IRremote.hpp>
+#include <LiquidCrystal.h>
+#include <IRremote.h>
 
-int RECV_PIN = 11;    
-int pinoledvermelho = 5;  
-int pinoledverde = 7;
+int RECV_PIN = 11;
 float eq[2][3];
-String leitura="";
-long x=0, y=0, a=0, b=0, n1=0, n2=0;
-bool f=false;
+float teste = 523.56325;
+String leitura = "", l1 = "", l2 = "";
+long x = 0, y = 0, a = 0, b = 0, n1 = 0, n2 = 0;
+bool f = false;
+const int rs = 3, en = 4, d4 = A2, d5 = A3, d6 = A4, d7 = A5;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-void setup()  
-{  
-  pinMode(pinoledvermelho, OUTPUT);   
-  pinMode(pinoledverde, OUTPUT);  
-  Serial.begin(9600);  
+void setup()
+{
+  Serial.begin(9600);
+  lcd.begin(16, 2);
   IrReceiver.begin(RECV_PIN);
   randomSeed(analogRead(0));
 }
 //Função 1: para leitura dos numeros IR
-void lerNumeros(){
-  if(IrReceiver.decode()){
-    auto dado= IrReceiver.decodedIRData.decodedRawData;
+void lerNumeros() {
+  if (IrReceiver.decode()) {
+    auto dado = IrReceiver.decodedIRData.decodedRawData;
     Serial.println(dado);
-    if(f==false){
-      f=true;
-      Serial.println("Aperte *!");
+    if (f == false) {
+      f = true;
+      n1 = random(1, 10);
+      n2 = random(1, 10);
+      faseUm(n1, n2);
+      exibirLcd("Aperte EQ!", "...");
       delay(1000);
-      Serial.println("Gerando Equação...");
+      exibirLcd(l1, l2);
     }
     delay(500);
-    switch(dado){
-        case 3860463360:
-        leitura+= "0";
+    switch (dado) {
+      case 3860463360:
+        leitura += "0";
         break;
-        case 3125149440:
-        leitura+="1";
+      case 3125149440:
+        leitura += "1";
         break;
-        case 3108437760:
-        leitura+="2";
+      case 3108437760:
+        leitura += "2";
         break;
-        case 3091726080:
-        leitura+="3";
+      case 3091726080:
+        leitura += "3";
         break;
-        case 3141861120:
-        leitura+="4";
+      case 3141861120:
+        leitura += "4";
         break;
-        case 3208707840:
-        leitura+="5";
+      case 3208707840:
+        leitura += "5";
         break;
-        case 3158572800:
-        leitura+="6";
+      case 3158572800:
+        leitura += "6";
         break;
-        case 4161273600:
-        leitura+="7";
+      case 4161273600:
+        leitura += "7";
         break;
-        case 3927310080 :
-        leitura+="8";
+      case 3927310080 :
+        leitura += "8";
         break;
-        case 4127850240:
-        leitura+="9";
-        case 3810328320:
-        Serial.print("Digitou: ");
-        Serial.println(leitura);
-        Serial.println("X << ou >> Y");
+      case 4127850240:
+        leitura += "9";
         break;
-        case 4144561920:
-        Serial.print("Adicionado >> x= ");
-        x=converteNumero(leitura);
-        Serial.println(x);
-        leitura="";
+      case 3810328320:
+        exibirLcd("Digitou: ", leitura);
+        exibirLcd("X << ou >> Y", "---escolha---");
         break;
-        case 2774204160:
-        Serial.print("Adicionado >> y= ");
-        y=converteNumero(leitura);
-        Serial.println(y);
-        leitura="";
+      case 4144561920:
+        x = converteNumero(leitura);
+        exibirLcd("Adicionado>>X:", leitura);
+        leitura = "";
         break;
-        case 4061003520:
-        n1=random(1,10);
-        n2=random(1,10);
-        faseUm(n1, n2);
-        Serial.println("[OK] X e Y");
-        Serial.println("[UP] Resposta");
+      case 2774204160:
+        y = converteNumero(leitura);
+        exibirLcd("Adicionado>>Y:", leitura);
+        leitura = "";
         break;
-        case 3877175040:
+      case 4061003520:
+        exibirLcd("[Play] X e Y", "[UP] Resposta");
+        break;
+      case 3877175040:
         resolveEquacao();
-      if(x==eq[0][1] && y==eq[0][0]){
-        Serial.println("Acertou Miseravel");
-      }else{
-        Serial.println("Errouuuuuu!");
-      }
+        delay(1500);
+        if (y == eq[0][1] && x == eq[0][0]) {
+          exibirLcd("Acertou", "Miseravel!!");
+        } else {
+          exibirLcd("Errouuu...", "Errourude!!");
+        }
         break;
     }
     IrReceiver.resume();
@@ -96,44 +95,49 @@ void lerNumeros(){
   }
 }
 //Função 2: de converter em numero
-int converteNumero(String teclado){
+int converteNumero(String teclado) {
   return teclado.toInt();
 }
 //Função 4: Irá exibir a Matriz da Equacao
 void exibirMatriz(){
-  Serial.print(eq[0][0]);
-  Serial.print("x");
-  Serial.print(" + ");
-  Serial.print(eq[0][1]);
-  Serial.print("y");
-  Serial.print(" = ");
-  Serial.println(eq[0][2]);
-  Serial.print(eq[1][0]);
-  Serial.print("x");
-  Serial.print(" + ");
-  Serial.print(eq[1][1]);
-  Serial.print("y");
-  Serial.print(" = ");
-  Serial.println(eq[1][2]);
+  l1+=String(eq[0][0],0);
+  l1+="x+";
+  l1+=String(eq[0][1],0);
+  l1+="y=";
+  l1+=String(eq[0][2],0);
+  l2+=String(eq[1][0],0);
+  l2+="x+";
+  l2+=String(eq[1][1],0);
+  l2+="y=";
+  l2+=String(eq[1][2],0);
 }
 //Função5: Resolve a Equacao
-void resolveEquacao(){
-  eq[0][1]=((eq[1][0]*eq[0][2])-eq[1][2])/((eq[1][0]*eq[0][0])-eq[1][1]);
-  eq[0][0]=(eq[0][2]-eq[0][1]);
+void resolveEquacao() {
+  eq[0][1] = ((eq[1][0] * eq[0][2]) - eq[1][2]) / ((eq[1][0] * eq[0][0]) - eq[1][1]);
+  eq[0][0] = (eq[0][2] - eq[0][1]);
   exibirMatriz();
 }
 //Função 6: Monta a Matriz da Equação
-void faseUm(long x, long y){
-  a=random(1,10);
-  b=random(1,10);
-  eq[0][0]=1;
-  eq[0][1]=1;
-  eq[0][2]=(x+y);
-  eq[1][0]=(a);
-  eq[1][1]=(b);
-  eq[1][2]=((a*x)+(b*y));
+void faseUm(long x, long y) {
+  a = random(1, 10);
+  b = random(1, 10);
+  eq[0][0] = 1;
+  eq[0][1] = 1;
+  eq[0][2] = (x + y);
+  eq[1][0] = (a);
+  eq[1][1] = (b);
+  eq[1][2] = ((a * x) + (b * y));
   exibirMatriz();
 }
-void loop(){
+void exibirLcd(String l1, String l2){
+  if(l1 && l2){
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print(l1);
+    lcd.setCursor(0,1);
+    lcd.print(l2);
+  }
+}
+void loop() {
   lerNumeros();
 }
